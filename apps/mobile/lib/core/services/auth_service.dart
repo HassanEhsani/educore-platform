5b3d9afca70b053dd33450f8a2948782fbf176cb
Hassan Ehsani <hassan.ehsani.am@gmail.com>
2026-07-19 11:32:01 +0430

    fix auth storage database initialization services

--- apps/mobile/lib/core/services/auth_service.dart
+++ apps/mobile/lib/core/services/auth_service.dart
@@ -6,29 +6,17 @@ class AuthService {
 
   static final AuthService instance = AuthService._();
 
-  final StorageService _storage = StorageService.instance;
+  bool _initialized = false;
 
-  static const String _tokenKey = 'auth_token';
-  static const String _userKey = 'current_user';
-
-  Future<void> saveToken(String token) async {
-    await _storage.setString(_tokenKey, token);
+  Future<void> init() async {
+    _initialized = true;
   }
 
-  String? getToken() {
-    return _storage.getString(_tokenKey);
-  }
-
-  Future<void> saveUser(UserModel user) async {
-    await _storage.setString(_userKey, user.toJson().toString());
+  bool get isInitialized {
+    return _initialized;
   }
 
   Future<void> logout() async {
-    await _storage.remove(_tokenKey);
-    await _storage.remove(_userKey);
-  }
-
-  bool get isAuthenticated {
-    return getToken() != null;
+    _initialized = false;
   }
 }
